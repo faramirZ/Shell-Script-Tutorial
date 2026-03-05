b1d8cc272267e141fd71eb72ffaaf69dea487aa1
faram <faramirzailan@gmail.com>
2026-03-05 14:15:24 +0800

    add redo.sh

---
+++ exercise-data/alkanes/redo-figure-3.sh
@@ -0,0 +1,5 @@
+  389  bash sorted.sh *.pdb ../creatures/*.dat
+  390  git add .
+  391  git commit -m 'middle.sh fix and sorted bash added'
+  392  git push origin master
+  393  history | tail -n 5 > redo-figure-3.sh
